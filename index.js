c81a0e374917e8c6ec8436e84a194b0da102e61b
Ayesha Ilyas <annie16950@gmail.com>
2024-12-12 22:37:27 -0500

    add ability to filter games based on funding status and connect filter code to buttons

--- index.js
+++ index.js
@@ -97,10 +97,10 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
-
+    const unfunded = GAMES_JSON.filter(game => game.pledged < game.goal);
+    console.log("unfunded " + unfunded.length);
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfunded);
 }
 
 // show only games that are fully funded
@@ -108,18 +108,19 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
-
+    const funded = GAMES_JSON.filter(game => game.pledged >= game.goal);
+    console.log("funded " + funded.length);
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(funded);
 }
+
 
 // show all games
 function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -128,6 +129,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", e => filterUnfundedOnly());
+fundedBtn.addEventListener("click", e => filterFundedOnly());
+allBtn.addEventListener("click", e => showAllGames());
 
 
 /*************************************************************************************
